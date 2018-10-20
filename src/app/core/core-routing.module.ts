0ec2f5f878948acb4636b363c6c7dd86ea2db6a7
Bernardo Baumblatt <baumblatt@gmail.com>
2018-10-20 01:02:37 -0300

    Layout da tela de login

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LayoutComponent} from './container/layout/layout.component';
+import {LoginComponent} from './container/login/login.component';
 
-const routes: Routes = [{
-	path: '', component: LayoutComponent
-}];
+const routes: Routes = [
+	{path: 'login', component: LoginComponent},
+	{path: '', component: LayoutComponent}
+];
 
 @NgModule({
 	imports: [RouterModule.forChild(routes)],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,20 +4,22 @@ import {CommonModule} from '@angular/common';
 import {CoreRoutingModule} from './core-routing.module';
 import {LayoutComponent} from './container/layout/layout.component';
 import {LayoutModule} from '@angular/cdk/layout';
-import {MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule} from '@angular/material';
+import {MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule} from '@angular/material';
+import {LoginComponent} from './container/login/login.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		CoreRoutingModule,
 		LayoutModule,
+		MatCardModule,
 		MatToolbarModule,
 		MatButtonModule,
 		MatSidenavModule,
 		MatIconModule,
 		MatListModule
 	],
-	declarations: [LayoutComponent]
+	declarations: [LayoutComponent, LoginComponent]
 })
 export class CoreModule {
 }
